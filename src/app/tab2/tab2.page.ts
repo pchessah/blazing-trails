22a037fb2fab7f3981c82c0d6913f6a027a8eb12
Paul Chesa <paulchesa1@gmail.com>
2023-11-02 10:40:52 +0300

    Mock adding items

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+import { IItem } from '../interfaces/item.interface';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Tab2Page {
 
-  constructor() {}
+  @ViewChild(IonModal) modal!: IonModal;
+
+  item: IItem = {} as any;
+  itemList: IItem[] = [];
+
+  constructor(private _cd:ChangeDetectorRef){}
+
+  cancel() {
+    this.item = undefined as any;
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.item, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<IItem>>;
+    if (ev.detail.role === 'confirm' && !!this.item) {
+      this.itemList.push(this.item)
+      this.item = {} as any
+      this._cd.detectChanges()
+    }
+  }
+
+  onInput(event: CustomEvent, input: 'qty' | 'item'){
+  
+    if(input === 'qty'){
+      this.item['quantity'] = event.detail.value;
+    } else if (input === 'item'){
+      this.item['name'] = event.detail.value.toLowerCase();
+    }
+  }
 
 }
