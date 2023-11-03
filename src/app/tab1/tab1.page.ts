fd6d7e1cef13eec0bbea2cae248a5c635dcc08eb
Paul Chesa <paulchesa1@gmail.com>
2023-11-03 09:17:38 +0300

    Get data from service

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RecentUpdatesService } from '../services/recent-updates.service';
+import { Observable } from 'rxjs';
+import { IRecentUpdate } from '../interfaces/recent-update.interface';
+import { InventoryFireStoreService } from '../services/inventory-firestore.service';
+import { IItem } from '../interfaces/item.interface';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,11 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  private _recentUpdateService:RecentUpdatesService = inject(RecentUpdatesService);
+  private _inventoryFiresStoreService:InventoryFireStoreService = inject(InventoryFireStoreService);
+
+  recentUpdates$: Observable<IRecentUpdate[]> = this._recentUpdateService.getRecentUpdates();
+  lowInventory$: Observable<IItem[]> = this._inventoryFiresStoreService.lowInventory$;
+  inventory$: Observable<IItem[]> = this._inventoryFiresStoreService.getinventory()
 
 }
