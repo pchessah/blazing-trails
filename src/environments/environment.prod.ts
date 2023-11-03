9379a2104a174db493fb8813e59ed2d94ed6baab
Paul Chesa <paulchesa1@gmail.com>
2023-11-03 19:41:37 +0300

    Added android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey:"AIzaSyBWwyLMJqsCsosDr16w0iPobNV89eOOMNE",
+    authDomain:"blazing-trails-cd66e.firebaseapp.com",
+    projectId: "blazing-trails-cd66e",
+    storageBucket:"blazing-trails-cd66e.appspot.com",
+    messagingSenderId:"252074081141",
+    appId:"1:463416303148:web:b7a2d6b5b6646480c3bb89",
+    measurementId:"463416303148",
+    databaseURL: "https://blazing-trails-cd66e-default-rtdb.europe-west1.firebasedatabase.app",
+  }
 };
