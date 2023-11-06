2aeccb3dba3d9ebd1a12bc28a9efddd1cc869e2b
Paul Chesa <paulchesa1@gmail.com>
2023-11-06 05:56:15 +0300

    Created registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
   }
+
 ];
 @NgModule({
   imports: [
